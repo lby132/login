0e2655ca40896cf72b03b6ef45bae60301a8d8f6
pro <quddbscjswow@naver.com>
2022-02-21 23:35:36 +0900

    필터 추가

--- src/main/java/hello/login/ItemServiceApplication.java
+++ src/main/java/hello/login/ItemServiceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemServiceApplication {
+public class
+ItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
